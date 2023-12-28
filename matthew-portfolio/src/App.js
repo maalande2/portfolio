17a0ed0e70a32e1c21dfd7700a7055a061749f0c
Matthew Anderson <maalande2@gmail.com>
2023-12-28 14:14:39 -0800

    initial commit

---
+++ matthew-portfolio/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ucscTree from './imgs/ucsc-tree.jpg';
+
+function App() {
+  return (
+    <div className="flex-none bg-red-500 p-32">
+      aaaaa
+    </div>
+  );
+}
+
+export default App;
