60259af1491c40d4ed30d7ab8a72a4a3f46215e3
Matthew Anderson <maalande2@gmail.com>
2024-01-10 17:13:50 -0800

    a

--- matthew-portfolio/src/App.js
+++ matthew-portfolio/src/App.js
@@ -3,10 +3,27 @@ import ucscTree from './imgs/ucsc-tree.jpg';
 
 function App() {
   return (
-    <div className="flex-none bg-red-500 p-32">
-      aaaaa
+    <div className="App bg-cover bg-center min-h-screen min-w-full" style={{ backgroundImage: `url(${ucscTree})` }}>
+      <div className="App-header flex-none">
+        <p className="text-3xl">Matthew Anderson</p>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
+// import React from 'react';
+
+// const MyComponent = () => {
+//   return (
+//     <div className="flex justify-center items-center h-screen">
+//       <div className="bg-blue-500 text-white font-bold rounded-lg border shadow-lg p-10">
+//         This is a React component styled with Tailwind CSS!
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default MyComponent;
